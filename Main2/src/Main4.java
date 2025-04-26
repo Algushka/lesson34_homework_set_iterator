ce116ded4d8b954f5971603e8f707c162dafee05
ASUS <successolga118@gmail.com>
2025-04-26 21:13:46 +0300

    3 задачки( 3 задача двумя способами))

--- Main2/src/Main4.java
+++ Main2/src/Main4.java
@@ -1,5 +1,4 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 //задача 3 ( с внутренним итератором)
 //Дана строка с именами вида Jack,John,Nick,John.
 //Необходимо написать метод, который удалит из этой
@@ -11,6 +10,7 @@ import java.util.Set;
 public class Main4 {
     public static void main(String[] args) {
         String str ="John,Jack,Nick,Dick,Olga,Pick";
+        String str1="";
         str.trim();/* избавимся от лишних пробелов*/
         String [] words = str.split(",");
         Set<String> set = new HashSet<>();
@@ -19,6 +19,23 @@ public class Main4 {
         }
         System.out.println("Список без повторений:");
         set.forEach(s-> System.out.println(s));
+        List <String> list = setToList(set);
+        System.out.println("Cписок без повторений с запятыми:" + list);
+        list.sort(String::compareToIgnoreCase);
+        System.out.println("Сортировано по алфавиту"+list);
 
     }
+    public static List<String> setToList(Set <String> set) {
+        List <String> list = new ArrayList<>();
+        int index=0;
+        for (String element: set ) {
+            list.add(element);
+            index++;
+        }
+        return list;
+    }
+
+
+
+
 }
