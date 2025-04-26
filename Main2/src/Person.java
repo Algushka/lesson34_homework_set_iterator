b75ca9cd4c10acd8f8d5ee13e24b96cc92a39cfd
ASUS <successolga118@gmail.com>
2025-04-26 13:06:41 +0300

    2 задачка

---
+++ Main2/src/Person.java
@@ -0,0 +1,58 @@
+import java.util.Objects;
+
+public class Person {
+    private String fName;
+    private String lName;
+    private int age;
+
+    public Person(String fName, String lName, int age) {
+        this.fName = fName;
+        this.lName = lName;
+        this.age = age;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "fName='" + fName + '\'' +
+                ", lName='" + lName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && Objects.equals(fName, person.fName) && Objects.equals(lName, person.lName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(fName, lName, age);
+    }
+}
